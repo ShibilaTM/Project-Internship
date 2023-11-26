dcc0f31bf0764dfef195b4ea8fa4b5d7bb78fde0
roleson <rolesonjg96@gmail.com>
2023-11-26 17:14:23 +0530

    integrated student dashboard with home and login

--- frontend/src/ui-components/Login.jsx
+++ frontend/src/ui-components/Login.jsx
@@ -1,63 +1,69 @@
 
-import {Button,  Grid,  TextField } from '@mui/material'
+import { Button, Grid, TextField } from '@mui/material'
 import { Paper } from '@mui/material'
 import { Avatar } from '@mui/material'
 import LockPersonIcon from '@mui/icons-material/LockPerson';
 import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 
 const Login = () => {
-    const [user,setUser] = useState({
-    email:'',
-    password:''
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
   })
-const inputHandler=(e)=>{
-      setUser({
-        ...user,
-        [e.target.name]:e.target.value
-      })
-}
-
-
-
-const addHandler = async (e) => {
-  try {
-     axios.post('http://127.0.0.1:4000/user/login', user)
-     .then((res)=>{
-      alert(res.data.message)
-      if(res.data.message ==='success'){
-        sessionStorage.setItem('userToken',res.data.token)
-        console.log('successfully logged in')
-      }
-     })
-
-   
-  } catch (err) {
-    console.log(err);
+  const inputHandler = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
   }
-};
-  const paperStyle ={padding:20,height:'70vh',width:300,margin:'20px auto'}
-  const avatarStyle = {backgroundColor:'#BF40BF'}
-  const buttonStyle ={margin:'15px 0'}
+
+
+
+  const addHandler = async (e) => {
+
+    try {
+      axios.post('http://127.0.0.1:4000/user/login', user)
+        .then((res) => {
+          alert(res.data.message)
+          if (res.data.message === 'success') {
+            sessionStorage.setItem('userToken', res.data.token)
+            console.log('successfully logged in')
+            navigate("/studentdashboard")
+
+          }
+        })
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const paperStyle = { padding: 20, height: '70vh', width: 300, margin: '20px auto' }
+  const avatarStyle = { backgroundColor: '#BF40BF' }
+  const buttonStyle = { margin: '15px 0' }
   return (
     <Grid>
-      <Paper  style={paperStyle}>
+      <Paper style={paperStyle}>
         <Grid align="center">
-        <Avatar style={avatarStyle}><LockPersonIcon/></Avatar>
-              <h1 sx={{color:'purple'}}>Login</h1>
+          <Avatar style={avatarStyle}><LockPersonIcon /></Avatar>
+          <h1 sx={{ color: 'purple' }}>Login</h1>
         </Grid>
-     
-    <br />
-  <TextField variant='outlined' label='email' name='email' onChange={inputHandler} fullWidth/>
-    <br /><br />
-     <TextField variant='outlined' label='Password' name='password' type='password' onChange={inputHandler} fullWidth/>
-    <br /><br />
-   <Button variant='contained' color='secondary' style={buttonStyle} onClick={addHandler} fullWidth>Login</Button>
-    {/* <span>Email or password is incorrect</span> */}
-   </Paper>
+
+        <br />
+        <TextField variant='outlined' label='email' name='email' onChange={inputHandler} fullWidth />
+        <br /><br />
+        <TextField variant='outlined' label='Password' name='password' type='password' onChange={inputHandler} fullWidth />
+        <br /><br />
+        <Button variant='contained' color='secondary' style={buttonStyle} onClick={addHandler} fullWidth>Login</Button>
+        {/* <span>Email or password is incorrect</span> */}
+      </Paper>
     </Grid>
-      )
-    }
-    
-    export default Login
+  )
+}
+
+export default Login
