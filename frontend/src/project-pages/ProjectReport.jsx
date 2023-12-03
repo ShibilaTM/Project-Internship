099db3ccd3d57bb9fda1800c43c735d44d56f6a8
ShibilaTM <shibilaang@gmail.com>
2023-12-03 14:18:43 +0530

    first commit

--- frontend/src/project-pages/ProjectReport.jsx
+++ frontend/src/project-pages/ProjectReport.jsx
@@ -33,11 +33,7 @@ const ProjectReport = () => {
       alert('You have already submitted the form');
       return;
     }
-      // Submit only if the user has not submitted before
-      if (project.title.trim() === '' || project.url.trim() === '' || project.comments.trim() === ''||project.comments.trim() === '') {
-        alert('Please fill in all fields.');
-        return;
-      }
+      
 
     const userEmail = localStorage.getItem('userEmail');
      // Include the user's email in the payload
@@ -82,3 +78,102 @@ const ProjectReport = () => {
 };
 
 export default ProjectReport;
+
+
+// import { Button, TextField } from '@mui/material';
+// import React, { useState, useEffect } from 'react';
+// import './Submission.css';
+// import axiosInstance from '../axiosinterceptor';
+
+// const ProjectReport = () => {
+//   const [project, setProject] = useState({
+//     projecttitle: '',
+//     url: '',
+//     projectreport: '',
+//     comments: '',
+//   });
+
+//   const [alreadySubmitted, setAlreadySubmitted] = useState(false);
+
+//   useEffect(() => {
+
+//     const userEmail = localStorage.getItem('userEmail');
+//     // Check user submission status when the component mounts
+//     checkSubmissionStatus();
+//   }, []);
+
+//   const projectHandler = (e) => {
+//     setProject({ ...project, [e.target.name]: e.target.value });
+//   };
+
+//   const checkSubmissionStatus = () => {
+//     const userEmail = localStorage.getItem('userEmail');
+    
+//     // Use a server-side endpoint to check the user's submission status
+//     axiosInstance.get(`http://127.0.0.1:4000/form/submission-status?userEmail=${userEmail}`)
+//       .then((res) => {
+//         setAlreadySubmitted(res.data.hasSubmitted);
+//       })
+//       .catch((error) => {
+//         console.error('Error checking submission status:', error);
+//       });
+//   };
+
+//   const addProject = (e) => {
+//     // Check if the user has already submitted the form
+//     if (alreadySubmitted) {
+//       alert('You have already submitted the form');
+//       return;
+//     }
+
+//     // Submit only if the user has not submitted before
+//     // if (project.title.trim() === '' || project.url.trim() === '' ||project.projectreport.trim()==='' ||project.comments.trim() === '') {
+//     //   alert('Please fill in all fields.');
+//     //   return;
+//     // }
+
+//     const userEmail = localStorage.getItem('userEmail');
+    
+//     // Include the user's email in the payload
+//     const projectData = {
+//       ...project,
+//       userEmail: userEmail
+//     };
+
+//     // Use a server-side endpoint to submit the project
+//     axiosInstance.post('http://127.0.0.1:4000/form/project', projectData).then((res) => {
+//       alert(res.data.message);
+//       setAlreadySubmitted(true);
+//       // Save in local storage to persist the information across browser sessions (if needed)
+//       localStorage.setItem(`submitted_${userEmail}`, 'true');
+//     });
+//   };
+
+//   return (
+//     <div className='form'>
+//       <div className='input'>
+//         <h2>Final Project Submission</h2>
+
+//         <br />
+
+//         <TextField variant='outlined' label='Project Title' fullWidth name='projecttitle' onChange={projectHandler} />
+//         <br />
+//         <br />
+//         <TextField variant='outlined' label='URL' fullWidth name='url' onChange={projectHandler} />
+//         <br />
+//         <br />
+//         <TextField variant='outlined' label='Project Report' fullWidth name='projectreport' onChange={projectHandler} />
+//         <br />
+//         <br />
+//         <TextField variant='outlined' label='Comments' name='comments' fullWidth multiline rows={3} onChange={projectHandler} />
+//         <br />
+//         <br />
+//         <Button variant='contained' onClick={addProject}>
+//           Save & Submit
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProjectReport;
